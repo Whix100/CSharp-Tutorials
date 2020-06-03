54b5d4847dd2f2a8d0d31cc36ef1acc9491aff8b
Whix100 <whix100@gmail.com>
2020-06-03 17:41:00 -0400

    Container Controls
    Added the Container Controls project from the tutorial

---
+++ Container Controls/Container Controls/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Container_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExPanel = new System.Windows.Forms.Panel();
+            this.ExGroupBox = new System.Windows.Forms.GroupBox();
+            this.ExFlowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.ExTabControl = new System.Windows.Forms.TabControl();
+            this.ExTabPage1 = new System.Windows.Forms.TabPage();
+            this.ExTabPage2 = new System.Windows.Forms.TabPage();
+            this.ExTablePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ExSplitPanel = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.ExPanel.SuspendLayout();
+            this.ExGroupBox.SuspendLayout();
+            this.ExFlowPanel.SuspendLayout();
+            this.ExTabControl.SuspendLayout();
+            this.ExTabPage1.SuspendLayout();
+            this.ExTabPage2.SuspendLayout();
+            this.ExTablePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ExSplitPanel)).BeginInit();
+            this.ExSplitPanel.Panel1.SuspendLayout();
+            this.ExSplitPanel.Panel2.SuspendLayout();
+            this.ExSplitPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExPanel
+            // 
+            this.ExPanel.Controls.Add(this.button1);
+            this.ExPanel.Location = new System.Drawing.Point(12, 12);
+            this.ExPanel.Name = "ExPanel";
+            this.ExPanel.Size = new System.Drawing.Size(200, 100);
+            this.ExPanel.TabIndex = 0;
+            // 
+            // ExGroupBox
+            // 
+            this.ExGroupBox.Controls.Add(this.button4);
+            this.ExGroupBox.Location = new System.Drawing.Point(218, 118);
+            this.ExGroupBox.Name = "ExGroupBox";
+            this.ExGroupBox.Size = new System.Drawing.Size(200, 100);
+            this.ExGroupBox.TabIndex = 1;
+            this.ExGroupBox.TabStop = false;
+            this.ExGroupBox.Text = "groupBox1";
+            // 
+            // ExFlowPanel
+            // 
+            this.ExFlowPanel.Controls.Add(this.button2);
+            this.ExFlowPanel.Location = new System.Drawing.Point(218, 12);
+            this.ExFlowPanel.Name = "ExFlowPanel";
+            this.ExFlowPanel.Size = new System.Drawing.Size(200, 100);
+            this.ExFlowPanel.TabIndex = 2;
+            // 
+            // ExTabControl
+            // 
+            this.ExTabControl.Controls.Add(this.ExTabPage1);
+            this.ExTabControl.Controls.Add(this.ExTabPage2);
+            this.ExTabControl.Location = new System.Drawing.Point(218, 224);
+            this.ExTabControl.Name = "ExTabControl";
+            this.ExTabControl.SelectedIndex = 0;
+            this.ExTabControl.Size = new System.Drawing.Size(200, 106);
+            this.ExTabControl.TabIndex = 3;
+            // 
+            // ExTabPage1
+            // 
+            this.ExTabPage1.Controls.Add(this.button7);
+            this.ExTabPage1.Location = new System.Drawing.Point(4, 25);
+            this.ExTabPage1.Name = "ExTabPage1";
+            this.ExTabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.ExTabPage1.Size = new System.Drawing.Size(192, 77);
+            this.ExTabPage1.TabIndex = 0;
+            this.ExTabPage1.Text = "tabPage1";
+            this.ExTabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ExTabPage2
+            // 
+            this.ExTabPage2.Controls.Add(this.button8);
+            this.ExTabPage2.Location = new System.Drawing.Point(4, 25);
+            this.ExTabPage2.Name = "ExTabPage2";
+            this.ExTabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.ExTabPage2.Size = new System.Drawing.Size(192, 77);
+            this.ExTabPage2.TabIndex = 1;
+            this.ExTabPage2.Text = "tabPage2";
+            this.ExTabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ExTablePanel
+            // 
+            this.ExTablePanel.ColumnCount = 2;
+            this.ExTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ExTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ExTablePanel.Controls.Add(this.button3, 0, 0);
+            this.ExTablePanel.Location = new System.Drawing.Point(12, 118);
+            this.ExTablePanel.Name = "ExTablePanel";
+            this.ExTablePanel.RowCount = 2;
+            this.ExTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ExTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ExTablePanel.Size = new System.Drawing.Size(200, 100);
+            this.ExTablePanel.TabIndex = 4;
+            // 
+            // ExSplitPanel
+            // 
+            this.ExSplitPanel.Location = new System.Drawing.Point(12, 224);
+            this.ExSplitPanel.Name = "ExSplitPanel";
+            // 
+            // ExSplitPanel.Panel1
+            // 
+            this.ExSplitPanel.Panel1.Controls.Add(this.button5);
+            // 
+            // ExSplitPanel.Panel2
+            // 
+            this.ExSplitPanel.Panel2.Controls.Add(this.button6);
+            this.ExSplitPanel.Size = new System.Drawing.Size(200, 106);
+            this.ExSplitPanel.SplitterDistance = 89;
+            this.ExSplitPanel.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Panel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Flow";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Table";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(64, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Group";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Split";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Split";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(47, 29);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Tab";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(76, 34);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Tab";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Buttons_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 343);
+            this.Controls.Add(this.ExSplitPanel);
+            this.Controls.Add(this.ExTablePanel);
+            this.Controls.Add(this.ExTabControl);
+            this.Controls.Add(this.ExFlowPanel);
+            this.Controls.Add(this.ExGroupBox);
+            this.Controls.Add(this.ExPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Container Control";
+            this.ExPanel.ResumeLayout(false);
+            this.ExGroupBox.ResumeLayout(false);
+            this.ExFlowPanel.ResumeLayout(false);
+            this.ExTabControl.ResumeLayout(false);
+            this.ExTabPage1.ResumeLayout(false);
+            this.ExTabPage2.ResumeLayout(false);
+            this.ExTablePanel.ResumeLayout(false);
+            this.ExSplitPanel.Panel1.ResumeLayout(false);
+            this.ExSplitPanel.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ExSplitPanel)).EndInit();
+            this.ExSplitPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ExPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox ExGroupBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FlowLayoutPanel ExFlowPanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl ExTabControl;
+        private System.Windows.Forms.TabPage ExTabPage1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabPage ExTabPage2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TableLayoutPanel ExTablePanel;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SplitContainer ExSplitPanel;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
